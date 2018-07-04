377c1e399131177dd94f293b1607397ac10ff2bb
sharaththota <31200149+sharaththota@users.noreply.github.com>
2018-07-04 10:00:41 +0530

    Delete DeleteNode.py

--- Week-2/DeleteNode.py
+++
@@ -1,9 +0,0 @@
-import unittest
-def delete_node(node):
-    ptr = node.next
-    if ptr:
-        node.next = ptr.next
-        node.value = ptr.value
-    else:
-        raise ValueError("Bad input! Can't delete tail node.")
-
